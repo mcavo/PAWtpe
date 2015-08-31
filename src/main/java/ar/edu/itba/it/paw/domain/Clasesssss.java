01a18d701dd728c674c393d45013916012d7fca5
Victoria <mvictoria.cavo@gmail.com>
2015-08-31 19:19:16 -0300

    random classes

---
+++ src/main/java/ar/edu/itba/it/paw/domain/Clasesssss.java
@@ -0,0 +1,5 @@
+package ar.edu.itba.it.paw.domain;
+
+public class Clasesssss {
+
+}
---
+++ src/test/java/Start.java
@@ -0,0 +1,32 @@
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.webapp.WebAppContext;
+
+public class Start {
+	public static void main(String[] args) throws Exception {
+		Server server = new Server();
+		SocketConnector connector = new SocketConnector();
+		connector.setMaxIdleTime(1000 * 60 * 60);
+		connector.setSoLingerTime(-1);
+		connector.setPort(8000);
+		server.setConnectors(new Connector[] { connector });
+		WebAppContext bb = new WebAppContext();
+		bb.setServer(server);
+		bb.setContextPath("/paw");
+		bb.setWar("src/main/webapp");
+		server.addHandler(bb);
+		try {
+			System.out.println(">>> STARTING EMBEDDED JETTY SERVER, " + "PRESS ANY KEY TO STOP");
+			server.start();
+			while (System.in.available() == 0) {
+				Thread.sleep(5000);
+			}
+			server.stop();
+			server.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(100);
+		}
+	}
+}
