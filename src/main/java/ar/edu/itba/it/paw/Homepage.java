52054359797cbb24741842cdb95553a669145d5b
Julieta Sal-lari <julietasal-lari@MacBook-Pro-de-Julieta.local>
2015-09-07 01:05:14 -0300

    faltan los filtros: por eso la sesion ya abierta no muestra el mail

--- src/main/java/ar/edu/itba/it/paw/Homepage.java
+++ src/main/java/ar/edu/itba/it/paw/Homepage.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Homepage extends HttpServlet{
 
-	private String email;
-	private String pwd;
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -18,19 +16,14 @@ public class Homepage extends HttpServlet{
 		
 		UserManager userManager = new SessionUserManager(request);
 		if (userManager.existsUser()) {
-			System.out.println("en el if");
 			//email = userManager.getEmail();
 		}else{
-			email = request.getParameter("email");
-			pwd = request.getParameter("pwd");
-			/*if(!DBManager.getInstance().verifyUser(email, pwd)){
-				response.sendRedirect(response.encodeRedirectURL("login"));
-				return;
-			}
-			userManager.setUser(DBManager.getInstance().getUserId(email, pwd), pwd);*/
+			
+			
 		}
 		//response.setContentType("text/html");*/
-		request.setAttribute("email", email);
+
+		request.setAttribute("email", request.getAttribute("email"));
 		request.getRequestDispatcher("/WEB-INF/jsp/homepage.jsp").forward(request, response);
 		
 	}
@@ -39,22 +32,6 @@ public class Homepage extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(req, resp);
-	}
-	
-	public String getEmail(){
-		return email;
-	}
-	
-	public String getPwd() {
-		return pwd;
-	}
-	
-	public void setEmail(String email){
-		this.email = email;
-	}
-	
-	public void setPwd(String pwd){
-		this.pwd = pwd;
 	}
 
 }
--- src/main/java/ar/edu/itba/it/paw/Login.java
+++ src/main/java/ar/edu/itba/it/paw/Login.java
@@ -6,19 +6,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.mortbay.jetty.servlet.AbstractSessionManager.Session;
+
+import ar.edu.itba.it.paw.models.User;
+import ar.edu.itba.it.paw.services.UserService;
+
 
 public class Login extends HttpServlet{
 
+	private String email;
+	private String pwd;
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
 		UserManager userManager = new SessionUserManager(request);
 		if (userManager.existsUser()) {
-			request.getRequestDispatcher("homepage").forward(request, response);
+			response.sendRedirect("homepage");
 			return;
 		}
 		//response.setContentType("text/html");
 		request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		User user = null;
+		email = req.getParameter("email");
+		pwd = req.getParameter("pwd");
+		
+		user = UserService.getUser(email, pwd);
+
+		if(user == null){
+			resp.sendRedirect(resp.encodeRedirectURL("login"));
+			return;
+		}
+		UserManager userManager = new SessionUserManager(req);
+		userManager.setUser(UserService.getUserId(email));
+		req.setAttribute("email", email);
+		req.getRequestDispatcher("/WEB-INF/jsp/homepage.jsp").forward(req, resp);
 	}
 }
