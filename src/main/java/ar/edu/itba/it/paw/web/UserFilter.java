005c4460eadc510e0f3dc314e00de8a3b3e1c12a
Victoria <mvictoria.cavo@gmail.com>
2015-09-10 19:06:44 -0300

    navbar

---
+++ src/main/java/ar/edu/itba/it/paw/web/UserFilter.java
@@ -0,0 +1,34 @@
+package ar.edu.itba.it.paw.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class UserFilter implements Filter {
+
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+		//Ver si esta logueado
+		
+		//Crear objeto userrr
+		
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
