c8e098adcc243dc98610287b12bdbc849c710940
mariaeugenia <me.sakuda@hotmail.com>
2015-09-13 16:13:39 -0300

    Fix del show restaurant (usr = null)

--- src/main/java/ar/edu/itba/it/paw/servlets/ShowRestaurant.java
+++ src/main/java/ar/edu/itba/it/paw/servlets/ShowRestaurant.java
@@ -1,7 +1,6 @@
 package ar.edu.itba.it.paw.servlets;
 
 import java.io.IOException;
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -9,7 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ar.edu.itba.it.paw.models.Address;
+import ar.edu.itba.it.paw.SessionUserManager;
+import ar.edu.itba.it.paw.UserManager;
+import ar.edu.itba.it.paw.DAO.UserDAO;
 import ar.edu.itba.it.paw.models.Menu;
 import ar.edu.itba.it.paw.models.Restaurant;
 import ar.edu.itba.it.paw.models.Section;
@@ -20,7 +21,10 @@ public class ShowRestaurant extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		User usr = (User) req.getAttribute("user");
+		UserManager manager = new SessionUserManager(req); 
+		String userId = manager.getUser(); 
+		User usr = UserDAO.getUser(userId);
+		
 		//----------------------
 		
 		String name = req.getParameter("name");
