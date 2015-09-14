dfffece4c96a180c612335476926c640ea7520c2
Victoria <mvictoria.cavo@gmail.com>
2015-09-14 19:58:44 -0300

    remove unused imports

--- src/main/java/ar/edu/itba/it/paw/DAO/DBManager.java
+++ src/main/java/ar/edu/itba/it/paw/DAO/DBManager.java
@@ -4,10 +4,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 public class DBManager {
