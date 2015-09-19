2218412999227a687a293581894115fd0f70f9dc
mariaeugenia <me.sakuda@hotmail.com>
2015-09-19 20:48:46 -0300

    Adding hash and equals to dish

--- src/main/java/ar/edu/itba/it/paw/models/Dish.java
+++ src/main/java/ar/edu/itba/it/paw/models/Dish.java
@@ -45,4 +45,29 @@ public class Dish {
 		return this.id;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Dish)) {
+			return false;
+		}
+		Dish other = (Dish) obj;
+		if (id != other.id) {
+			return false;
+		}
+		return true;
+	}
 }
