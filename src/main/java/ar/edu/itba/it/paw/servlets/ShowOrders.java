164c1b6caf0800135b29094f205a2364373c822c
Victoria <mvictoria.cavo@gmail.com>
2015-09-20 21:46:43 -0300

    show order binding

--- src/main/java/ar/edu/itba/it/paw/servlets/ShowOrders.java
+++ src/main/java/ar/edu/itba/it/paw/servlets/ShowOrders.java
@@ -10,28 +10,26 @@ import javax.servlet.http.HttpServletResponse;
 
 import ar.edu.itba.it.paw.models.Order;
 import ar.edu.itba.it.paw.models.Restaurant;
+import ar.edu.itba.it.paw.models.User;
+import ar.edu.itba.it.paw.services.ManagerService;
 import ar.edu.itba.it.paw.services.OrderService;
 import ar.edu.itba.it.paw.services.RestService;
 
 @SuppressWarnings("serial")
 public class ShowOrders extends HttpServlet {
+	
+	private User usr = null;
+	private Restaurant rest = null;
+	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//Chequear si soy manager -> Homepage
-		//
-		String name = req.getParameter("name");
-		String street = req.getParameter("srt");
-		String number = req.getParameter("numb");
-		String neighborhood = req.getParameter("neigh");
-		String city = req.getParameter("city");
-		String province = req.getParameter("prov");
-		String floor = req.getParameter("flr");
-		String apartment = req.getParameter("apt");
-
-		Restaurant rest = RestService.getRestaurant(name, street, number, neighborhood, city, province, floor,
-				apartment);
+		usr = (User) req.getAttribute("user");
+		rest = ManagerService.getRestByManager(usr);
+		
 		req.setAttribute("rest", rest);
 		List<Order> olist = OrderService.getHistoryOrder(rest);
-		req.getRequestDispatcher("/WEB-INF/jsp/menuRestaurant.jsp").forward(req, resp);
+		req.setAttribute("olist", olist);
+		req.getRequestDispatcher("/WEB-INF/jsp/showOrders.jsp").forward(req, resp);
 	}
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
