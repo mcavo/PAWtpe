c962adb697b0501a4c475b37f740df3f500eb0b1
Julieta Sal-lari <julietasal-lari@MacBook-Pro-de-Julieta.local>
2015-10-11 00:30:29 -0300

    user interceptor - sin probar

---
+++ src/main/java/ar/edu/itba/it/paw/interceptors/UserInterceptor.java
@@ -0,0 +1,31 @@
+package ar.edu.itba.it.paw.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import ar.edu.itba.it.paw.SessionUserManager;
+import ar.edu.itba.it.paw.UserManager;
+
+public class UserInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object arg2) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("PRE");
+		
+		HttpServletRequest request = (HttpServletRequest) req;
+		UserManager userManager = new SessionUserManager(request);
+		if (userManager.existsUser()) {
+			//User user = UserService.getUserById(userManager.getUserId());
+			request.setAttribute("user", userManager.getUser());
+		}else{
+			request.setAttribute("user", null);
+		}
+		return false;
+	}
+
+}
