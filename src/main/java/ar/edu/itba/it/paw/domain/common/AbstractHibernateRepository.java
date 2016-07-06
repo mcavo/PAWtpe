2135b6e106dcdd48fa4fe7dca52da244fd50d8dc
Victoria <mvictoria.cavo@gmail.com>
2016-07-06 19:34:51 -0300

    Fix database connection

--- src/main/java/ar/edu/itba/it/paw/domain/common/AbstractHibernateRepository.java
+++ src/main/java/ar/edu/itba/it/paw/domain/common/AbstractHibernateRepository.java
@@ -22,13 +22,11 @@ public class AbstractHibernateRepository {
 		@SuppressWarnings("unchecked")
 		public <T> List<T> find(String hql, Object... params) {
 			Session session = getSession();
-			session.beginTransaction();
 			Query query = session.createQuery(hql);
 			for (int i = 0; i < params.length; i++) {
 				query.setParameter(i, params[i]);
 			}
 			List<T> list = query.list();
-			session.getTransaction().commit();
 			return list;
 		}
 
