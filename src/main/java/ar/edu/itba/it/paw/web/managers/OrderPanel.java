4685dc3c8522a2b29383d2247fbe753ee009872c
mariaeugenia <me.sakuda@hotmail.com>
2016-07-09 18:06:09 -0300

    Agergando el el uso de message para order panel

--- src/main/java/ar/edu/itba/it/paw/web/managers/OrderPanel.java
+++ src/main/java/ar/edu/itba/it/paw/web/managers/OrderPanel.java
@@ -2,41 +2,25 @@ package ar.edu.itba.it.paw.web.managers;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.StringResourceModel;
 
 import ar.edu.itba.it.paw.domain.address.Address;
-import ar.edu.itba.it.paw.domain.address.AddressRepositoryType;
 import ar.edu.itba.it.paw.domain.restaurant.Order;
-import ar.edu.itba.it.paw.domain.restaurant.OrderRepositoryType;
 import ar.edu.itba.it.paw.domain.users.User;
 
 
 @SuppressWarnings("serial")
-public class OrderPanel extends Panel{
+public class OrderPanel extends Panel {
 	
 	public OrderPanel(String id, Order order) {
 		super(id);
 		User user = order.getUser();
-		add(new Label("userName", user.getFirstName() +" "+ user.getLastName()));
+		add(new Label("userName", new StringResourceModel("user_name", this, new Model<User>(user))));
 		Address address = user.getAddress();
+		add(new Label("userAddress", new StringResourceModel("address", this, new Model<Address>(address))));
 		
-		int floor = 0;
-		if(address.getFloor() != null){
-			floor = address.getFloor();
-		}
-		String user_address = address.getStreet();
-		user_address.concat(" ").concat(String.valueOf(address.getNumber()));
-		if(floor != 0){
-			user_address.concat(" ").concat(String.valueOf(floor));
-		}
-		
-		if (address.getApartment() != null) {
-			user_address.concat(" ").concat(address.getApartment());
-		}
-		user_address.concat(" ").concat(address.getNeighborhood().getName());
-		add(new Label("userAddress", user_address));
-		
-		add(new Label("total", String.valueOf(order.getTotal())));
+		add(new Label("total", new StringResourceModel("total", this, new Model<Order>(order))));
 	}
 
 }
