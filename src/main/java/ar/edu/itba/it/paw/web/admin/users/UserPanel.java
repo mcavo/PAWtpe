62c080f00d4ef5fc2dbd17942b64bbd8fe872c92
mariaeugenia <me.sakuda@hotmail.com>
2016-07-11 00:38:30 -0300

    Agregando pretty time

--- src/main/java/ar/edu/itba/it/paw/web/admin/users/UserPanel.java
+++ src/main/java/ar/edu/itba/it/paw/web/admin/users/UserPanel.java
@@ -1,5 +1,7 @@
 package ar.edu.itba.it.paw.web.admin.users;
 
+import java.util.Locale;
+
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.Form;
@@ -9,6 +11,7 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import ar.edu.itba.it.paw.domain.users.User;
 import ar.edu.itba.it.paw.domain.users.UserRepositoryType;
@@ -31,7 +34,8 @@ public class UserPanel extends Panel {
 		blocked = user.getBlock();
 		System.out.println(blocked);
 		form.add(new Label("userName", new StringResourceModel("user_name", this, new Model<User>(user))));
-		String lastConnection =  user.getLastConnection() != null ?  user.getLastConnection().toString() : " - ";
+		PrettyTime prettyTime = new PrettyTime(new Locale("es"));
+		String lastConnection =  user.getLastConnection() != null ?  prettyTime.format(user.getLastConnection()) : " - ";
 		form.add(new Label("lastConnection", lastConnection));
 		
 		form.add(new CheckBox("blocked", new PropertyModel<Boolean>(this, "blocked")));
