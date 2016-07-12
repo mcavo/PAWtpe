349a07f12b3fb32e4c2c17c01497623f2f4812eb
Victoria <mvictoria.cavo@gmail.com>
2016-07-12 18:07:29 -0300

    Some changes

--- src/main/java/ar/edu/itba/it/paw/web/profile/ProfilePage.java
+++ src/main/java/ar/edu/itba/it/paw/web/profile/ProfilePage.java
@@ -71,7 +71,7 @@ public class ProfilePage extends BasePage {
 		email = loggedUser.getEmail();
 		birth = loggedUser.getBirth();
 		day = DateService.getDayOfMonth(loggedUser.getBirth());
-		month = DateService.getMonth(loggedUser.getBirth())+1;
+		month = DateService.getMonth(loggedUser.getBirth());
 		year = DateService.getYear(loggedUser.getBirth());
 		address = loggedUser.getAddress();
 		street = loggedUser.getAddress().getStreet();
